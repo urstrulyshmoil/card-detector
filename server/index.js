b2f6f2b9c50d1c3eedde85ad7213488ee852d5a4
urstrulyshmoil <shmoilowaisk@gmail.com>
2026-04-04 07:56:47 +0530

    fix cors for production

--- server/index.js
+++ server/index.js
@@ -6,11 +6,12 @@ const detectRoute = require("./routes/detect");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use("/api/detect", detectRoute);
 
 app.get("/", (req, res) => res.json({ status: "Card Detector API running 🃏" }));
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
